ad9f4fcfc81206e09750c3c6af9a29ff108679e1
Tanmay-Surve <survetanmay710@gmail.com>
2025-07-16 12:12:59 +0530

    feat: define CourseDocument

---
+++ src/main/java/com/tanmay/courseapp/document/CourseDocument.java
@@ -0,0 +1,39 @@
+package com.tanmay.courseapp.document;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Data
+@Document(indexName = "courses")
+public class CourseDocument {
+
+    @Id
+    private String id;
+
+    @Field(type = FieldType.Search_As_You_Type)
+    private String title;
+
+    @Field(type = FieldType.Text)
+    private String description;
+
+    @Field(type = FieldType.Keyword)
+    private String category;
+
+    @Field(type = FieldType.Keyword)
+    private String type;
+
+    @Field(type = FieldType.Integer)
+    private Integer minAge;
+
+    @Field(type = FieldType.Integer)
+    private Integer maxAge;
+
+    @Field(type = FieldType.Double)
+    private Double price;
+
+    @Field(type = FieldType.Date)
+    private String nextSessionDate;
+}
